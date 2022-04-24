558cebd615f8fbbe4696dfa866d43b5cea2f7225
钟超 <zhongc@wingoht.com>
2022-04-24 13:33:15 +0800

    修改

--- src/extend/directives.js
+++ src/extend/directives.js
@@ -19,89 +19,41 @@ export default {
         el.removeEventListener('input', el.handler, false)
       },
     })
-    Vue.directive('toIsMax', { //只输入数字并且设置最大值
-      bind(el, binding) {
-        let regExp = /[^-0-9]/g;
-        el.handler = () => {
-          console.log(el.value)
-          el.value = el.value.replace(regExp, '');
-          if (el.value.length > binding.value[0] || el.value > binding.value[1]) {
-            el.value = binding.value[1]
-          }
-        }
-        el.addEventListener('input', el.handler, false)
-      },
-      unbind(el) {
-        el.removeEventListener('input', el.handler, false)
-      },
-    })
-    Vue.directive('toIsEmail', { //只输入数字
-      bind(el, binding) {
-        let regExp = /[^-0-9]/g;
-        el.handler = () => {
-          el.value = el.value.replace(regExp, '');
-        }
-        el.addEventListener('input', el.handler, false)
-      },
-      unbind(el) {
-        el.removeEventListener('input', el.handler, false)
-      },
-    })
-    Vue.directive('toIsAmount', { //金额（小数点无限制）
-      bind(el, binding) {
-        let regExp = /[^-0-9.]/g;
-        el.handler = () => {
-          let index = el.value.indexOf('.');
-          let prefix = '';
-          let value = ''
-          if (index === -1) {
-            value = el.value
-          } else {
-            if (el.value.match(/^(\.|-\.)/)) {
-              prefix = index ? '-0' : '0';
-            }
-            value = prefix + el.value.slice(0, index + 1) + el.value.slice(index).replace(/\./g, '');
-          }
 
-          el.value = value.replace(regExp, '');
-        }
-        el.addEventListener('input', el.handler, false)
-      },
-      unbind(el) {
-        el.removeEventListener('input', el.handler, false)
-      },
-    })
-    Vue.directive('toFixed', { //金额（自定义位数）
-      bind(el, binding) {
-        let regExp = /[^-0-9.]/g;
-        el.handler = () => {
-          let index = el.value.indexOf('.');
+    Vue.directive('toFixed', {//v-toFixed="[7,2]"
+      inserted(el, binding, vNode) {
+        let regExp = /[^0-9.]/g;
+        el.handler = (e) => {
+          let index = e.target.value.indexOf('.');
           let prefix = '';
-          let value = ''
+          let inputValue = ''
           if (index === -1) {
-            value = el.value
+            inputValue = String(Number(e.target.value))
           } else {
-            if (el.value.match(/^(\.|-\.)/)) {
+            if (e.target.value.match(/^(\.|-\.)/)) {
               prefix = '0'
             }
-            value = prefix + el.value.slice(0, index + 1) + el.value.slice(index).replace(/\./g, '');
+            inputValue = prefix + e.target.value.slice(0, index + 1) + e.target.value.slice(index).replace(/\./g, '').slice(0, 2);
           }
-          let totalNum = binding.value[0] + binding.value[1] + 1
-          el.value = value.replace(regExp, '');
-          if (el.value.indexOf('.') !== -1) {
-            if (el.value.length > totalNum) {
-              el.value = String(el.value).substring(0, el.value.length - 1)
+          let totalLength = binding.value[0] + binding.value[1] + 1
+          e.target.value = inputValue.replace(regExp, '');
+          if (e.target.value.indexOf('.') !== -1) {
+            if (e.target.value.length > totalLength) {
+              e.target.value = String(e.target.value).substring(0, e.target.value.length - 1)
             }
           } else {
-            if (el.value.length > binding.value[0]) {
-              el.value = String(el.value).substring(0, el.value.length - 1)
+            if (e.target.value.length > binding.value[0]) {
+              e.target.value = String(e.target.value).substring(0, e.target.value.length - 1)
             }
           }
+          const eve = document.createEvent('HTMLEvents')
+          eve.initEvent('input', true, true)
+          vNode.tag === "input" ? el.dispatchEvent(eve) : el.querySelector("input").dispatchEvent(eve)
         }
-        el.addEventListener('input', el.handler, false)
+        el.addEventListener('keyup', el.handler)
       },
       unbind(el) {
-        el.removeEventListener('input', el.handler, false)
+        el.removeEventListener('keyup', el.handler)
       },
     })
   },
